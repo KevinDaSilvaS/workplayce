a020fcd7874c3a8b3b6c0e245d18050a952a1821
Your Name <kevin.kds80@gmail.com>
2022-08-13 17:40:11 +0000

    add get and post places controller functions

--- apps/server/lib/server/controllers/places_controller.ex
+++ apps/server/lib/server/controllers/places_controller.ex
@@ -2,17 +2,34 @@ defmodule Server.PlacesController do
   use Server, :controller
 
   def index(conn, opts) do
-    body = Server.Integrations.Places.get_places("country like this")
+    {limit, page} = Server.Helpers.Pagination.set(opts)
+    filters = Server.Mappers.Places.map_filters(opts)
+
+    body =
+      Server.Integrations.Places.get_places(filters,
+        limit: limit,
+        skip: (page - 1) * limit
+      )
+
     conn |> put_status(200) |> json(body)
   end
 
   def show(conn, opts) do
-    conn |> put_status(200) |> json(%{})
+    place = Server.Integrations.Places.get_place(opts["id"])
+    conn |> put_status(200) |> json(place)
   end
 
   def create(conn, opts) do
-    body = Server.Integrations.Places.insert_place(%{})
-    conn |> put_status(200) |> json(body)
+    request_body = Server.Mappers.Places.validate_fields(opts)
+
+    case request_body do
+      {:ok, request_body} ->
+        body = Server.Integrations.Places.insert_place(request_body)
+        conn |> put_status(201) |> json(body)
+
+      {:error, err} ->
+        conn |> put_status(400) |> json(%{error: err})
+    end
   end
 
   def update(conn, opts) do
