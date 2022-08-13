323be565c79795922bf7f29be631450d9f4eee23
Your Name <kevin.kds80@gmail.com>
2022-08-13 17:42:11 +0000

    add integration functions

--- apps/server/lib/server/integrations/places.ex
+++ apps/server/lib/server/integrations/places.ex
@@ -1,6 +1,10 @@
 defmodule Server.Integrations.Places do
-  def get_places(filters) do
-    Places.Repository.Places.list_places(filters)
+  def get_places(filters, opts) do
+    Places.Repository.Places.list_places(filters, opts)
+  end
+
+  def get_place(id) do
+    Places.Repository.Places.get_one_place(id)
   end
 
   def insert_place(place) do
