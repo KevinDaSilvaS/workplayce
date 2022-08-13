8dafa614926c2b58475322b4be625ee743cf31f3
Your Name <kevin.kds80@gmail.com>
2022-08-13 17:39:16 +0000

    add mongodb to project

--- apps/places/lib/places/application.ex
+++ apps/places/lib/places/application.ex
@@ -3,11 +3,19 @@ defmodule Places.Application do
   # for more information on OTP Applications
   @moduledoc false
 
+  ## {:ok, conn} = Mongo.start_link(url: "mongodb://172.17.0.3:27017/notifications", username: "user", password: "12345")
   use Application
 
   @impl true
   def start(_type, _args) do
     children = [
+      {Mongo,
+       [url: "mongodb://172.17.0.3:27017/admin",
+       username: "user",
+       password: "12345",
+       auth_source: "admin",
+       auth_mecanism: "SCRAM-SHA-1",
+       name: Mongo.Places]}
       # Starts a worker by calling: Places.Worker.start_link(arg)
       # {Places.Worker, arg}
     ]
