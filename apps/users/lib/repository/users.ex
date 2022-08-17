ca860a7d0213bd8366d4ef15dda10325d3ff8e66
Your Name <kevin.kds80@gmail.com>
2022-08-17 22:30:18 +0000

    add users app

---
+++ apps/users/lib/repository/users.ex
@@ -0,0 +1,43 @@
+defmodule Users.Repository.Users do
+  def list_users(filters, opts) do
+    Mongo.find(Mongo.Users, "users", filters, opts)
+    |> Enum.to_list()
+    |> Enum.map(fn user ->
+      Map.put(user, "_id", BSON.ObjectId.encode!(user["_id"]))
+    end)
+  end
+
+  def insert_user(user) do
+    {:ok, insertedOneResult} = Mongo.insert_one(Mongo.Users, "users", user)
+
+    %{"inserted_id" => BSON.ObjectId.encode!(insertedOneResult.inserted_id)}
+  end
+
+  def get_one_user(user_id) do
+    user_id = BSON.ObjectId.decode!(user_id)
+    user = Mongo.find_one(Mongo.Users, "users", %{"_id" => user_id})
+    case user do
+      nil -> nil
+      _ -> Map.put(user, "_id", BSON.ObjectId.encode!(user["_id"]))
+    end
+  end
+
+  def update_user(replace_data, user_id) do
+    user_id = BSON.ObjectId.decode!(user_id)
+
+    Mongo.find_one_and_update(
+      Mongo.Users,
+      "users",
+      %{"_id" => user_id},
+      %{"$set": reuser_data})
+  end
+
+  def delete_user(user_id) do
+    user_id = BSON.ObjectId.decode!(user_id)
+
+    Mongo.find_one_and_delete(
+      Mongo.Users,
+      "users",
+      %{"_id" => user_id})
+  end
+end
