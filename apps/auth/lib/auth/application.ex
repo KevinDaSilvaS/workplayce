89b597573a8da4774c79c51fa01a8bb0f401f755
Your Name <kevin.kds80@gmail.com>
2022-08-19 23:33:49 +0000

    add expiration setter

--- apps/auth/lib/auth/application.ex
+++ apps/auth/lib/auth/application.ex
@@ -8,6 +8,14 @@ defmodule Auth.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      {Mongo,
+       [url: "mongodb://172.17.0.3:27017/admin",
+       username: "user",
+       password: "12345",
+       auth_source: "admin",
+       auth_mecanism: "SCRAM-SHA-1",
+       name: Mongo.Auth]},
+      {Expiration.Setter, 120}
       # Starts a worker by calling: Auth.Worker.start_link(arg)
       # {Auth.Worker, arg}
     ]
