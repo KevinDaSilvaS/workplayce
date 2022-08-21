25ca0520eca3b168bdd4c797eefde261a060a0ac
Your Name <kevin.kds80@gmail.com>
2022-08-21 23:12:27 +0000

    change password hashing

--- apps/server/lib/server/services/hash_passwords.ex
+++ apps/server/lib/server/services/hash_passwords.ex
@@ -1,5 +1,5 @@
 defmodule Server.Services.HashPasswords do
   def hash_password(password) do
-    :crypto.hash(:sha256, "123") |> Base.encode16()
+    :crypto.hash(:sha256, password) |> Base.encode16()
   end
 end
